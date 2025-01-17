87454a350973354f425ca753e76458e73d08881b
The-A-Team-dot <moserhellshock@gmail.com>
2025-01-17 19:14:15 +0200

    Add project files.

---
+++ Intelliponics/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Intelliponics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
